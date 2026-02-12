9ff7c935ca742dbbcd9712fdbeb75343ef86991c
Rachel Coffey <rachelc7050@gmail.com>
2026-02-11 19:44:42 -0700

    created view components - home, login, about, create, poll

---
+++ src/create/create.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Create() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>create displayed here</div>
+    </main>
+  );
+}
---
+++ src/poll/poll.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Poll() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>poll displayed here</div>
+    </main>
+  );
+}
