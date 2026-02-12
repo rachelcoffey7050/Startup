96fb2a895727c0796b0987dc57d9f62b6edf07a1
Rachel Coffey <rachelc7050@gmail.com>
2026-02-11 20:30:16 -0700

    created router

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Poll } from './poll/poll';
+import { Create } from './create/create';
+import { About } from './about/about';
+import { Home } from './home/home';
+
 
 export default function App() {
-  return <div ><header>
+  return (
+   <BrowserRouter>
+   <div >
+    <header>
       <h1>StoryPolls</h1>
       <nav>
         <menu>
-          <li className='nav-item'><a href="index.html">Home</a></li>
-          <li className='nav-item'><a href="about.html">About</a></li>
-          <li className='nav-item'><a href="create.html">Create Poll</a></li>
-          <li className='nav-item'><a href="login.html">Log In/Sign Up</a></li> 
+          <li className='nav-item'><NavLink href="index">Home</NavLink></li>
+          <li className='nav-item'><NavLink href="about">About</NavLink></li>
+          <li className='nav-item'><NavLink href="create">Create Poll</NavLink></li>
+          <li className='nav-item'><NavLink href="login">Log In/Sign Up</NavLink></li> 
         </menu>
       </nav>
       <hr />
     </header>
     
-    <main>App components go here</main>
+    <Routes>
+    <Route path='/' element={<Home />} exact />
+    <Route path='/poll' element={<Poll />} />
+    <Route path='/create' element={<Create />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='*' element={<NotFound />} />
+    </Routes>
 
     <footer>
       <span class="text-reset">Rachel Coffey</span>
       <a href="https://github.com/rachelcoffey7050/Startup">Source</a>
     </footer>
-    
     </div>;
+    </BrowserRouter>
+    );
+}
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
